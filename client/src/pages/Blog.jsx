c42360ba5cf179862eadbbe7a6e695e08e4e375f
Laiba Mehar <laibameharlaibamehar84@gmail.com>
2026-05-03 13:17:40 -0700

    updated blog and dependencies

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { assets, blog_data } from '../assets/assets'
+import Moment from 'moment'
 const Blog = () => {
-  return (
+  const {id} = useParams()
+  const [data, setdata] = useState(null)
+  const fetchBlogData = async ()=>{
+   const data = blog_data.find(item => item._id == id)
+   setdata(data)
+  }
+  useEffect (() => {
+fetchBlogData()
+  },[])
+  return  data ? (
+    <div className='relative'> 
+    <img src={assets.gradientBackground} alt=""  className='absolute -top-50 -z-1
+    opacity-50'/>
+      
+
     <div>
-      <h1>Blog</h1>
+      <p>Published on {Moment(data.createdAt).format('MMMM Do YYYY')} </p>
     </div>
-  )
+    <div></div>
+    </div>
+  ) : <div>Loding...</div>
 }
 
 export default Blog
