715a5bb8d6cde02d0649bef4ac1f1f2dcd36e3df
hira <hiranaveed033@gmail.com>
2026-05-13 15:27:47 +0500

    update code

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,4 +1,6 @@
-
+import fs from 'fs'
+import imagekit from '../configs/imagekit.js';
+import Blog from'../models/Blog.js';
 
 export const addBlog = async(req, res)=>{
     try{
@@ -11,9 +13,27 @@ export const addBlog = async(req, res)=>{
             return res.json({success: false, message: "Missing required fields"})
 
         }
+        const fileBuffer = fs.readFileSync(imagekit.path)
 
-        
-    }catch(error){
+        // upload Image to Imagekit
+    const response = await imagekit.upload({
+        file: fileBuffer,
+         fileName: imageFile.originalname,
+         folder: "/blogs"   
+        })
+        // optimization through imagekit URL transformation
+        const optimizedImageUrl = imagekit.url({
+            path: response.filepath,
+            transformation: [
+                // {quality: 'auto'}, Auto compression
+                // {format: 'webp'}, convert to modern format
+                // {width: '1280'} width resizing
+            ]
+        });
+        const image = optimizedImageUrl;
+        await blog.create({title, subtitle, description, category, image, isPublished})
+       res.json ({success: true, message:"Blog added successfully"}) 
+    }catch(error ) {
 
     }
 }
